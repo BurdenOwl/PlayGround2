74e0ee646f34dfd21a8f3c24aa2d967597b23181
BurdenOwl <sheevpalps252@gmail.com>
2024-11-14 23:21:15 +0900

    1/2 done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+const flag = '487bc9db81d4eadf4b859515291d2d2ad291ebafa58f615b32de345e52a69d9f';
+
+const hashData = async (data) => {
+    const encodedData = new TextEncoder().encode(data);
+    const hashBuffer = await crypto.subtle.digest("SHA-256", encodedData);
+    return Array.prototype.slice.call(new Uint8Array(hashBuffer)).map(b => b.toString(16).padStart(2, '0')).join('');
+};
+
 let lockDown = localStorage.getItem('whetherLockDown');
 if (lockDown == undefined || lockDown == null) {
     lockDown = "false";
@@ -9,4 +17,20 @@ if (lockDown === "true") {
     let locked2 = document.getElementById('lockDown2');
     locked.innerHTML = `Website blocked`;
     locked2.innerHTML = ``;
+};
+
+const submit = async () => {
+    const valueOf = document.getElementById('theText');
+    const actualValue = valueOf.value;
+
+    if (flag !== await hashData(actualValue)) {
+        alert("Incorrect. Now the website is locked");
+        lockDown = "true";
+        localStorage.setItem('whetherLockDown', lockDown);
+        window.location.href = atob('aHR0cHM6Ly90aW55dXJsLmNvbS9iZGRjcnZkYw==');
+        return;
+    };
+
+    const body = document.getElementById('lockDown');
+    body.innerHTML = "you win :)"
 };
